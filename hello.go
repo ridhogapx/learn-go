8caaad5c4224a966b200b68eb95a9313c612cee4
RageNeko26 <rneko2006@gmail.com>
2023-06-08 11:03:48 +0700

    Learn: Slices method

--- hello.go
+++ hello.go
@@ -27,6 +27,13 @@ func main() {
 	// Array
 	arr := [3]int{23, 1, 2006}
 	var arr2 = [...]string{"Foo", "Bar"}
+	fruits := [5]string{"Mango", "Orange", "Pinapple"}
+
+	arr2[1] = "Whooosh"
+
+	// Slices
+	slice_a := []int{5, 10, 20, 15, 7, 10}
+	slice_b := []string{"Orange", "Banana", "Apple"}
 
 	// Formating Output
 	fmt.Println(a + b)
@@ -35,5 +42,11 @@ func main() {
 	fmt.Printf("Does he was handsome? %v \n", is_tamvan)
 	fmt.Printf("The sound was %v \n", mystr)
 	fmt.Printf("Array: %v \n", arr)
-	fmt.Printf("Second array: %v \n", arr2)
+	fmt.Printf("Second element from 2nd array is: %v \n", arr2)
+	fmt.Printf("List of fruits: %v \n", len(fruits))
+	fmt.Printf("Slice A = %v \n", slice_a)
+	fmt.Printf("Slice B = %v \n", slice_b)
+	fmt.Printf("The length of Slice A: %v \n", len(slice_a))
+	fmt.Printf("The length of Slice B: %v \n", len(slice_b))
+
 }
