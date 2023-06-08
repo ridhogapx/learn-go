45a22086088dcb881d4bdb4317251fa337901dc6
RageNeko26 <rneko2006@gmail.com>
2023-06-08 22:53:49 +0700

    Learn: Grouped declaration

--- variable-declaration.go
+++ variable-declaration.go
@@ -13,5 +13,14 @@ func main() {
 
 	// Untyped
 	greet := "Welcome!"
-	fmt.Printf("He's said: %v", greet)
+	fmt.Printf("He's said: %v \n", greet)
+
+	var (
+		pet string = "Cat"
+		pet_name string = "Momosuke"
+		pet_age int = 1
+	)
+
+	fmt.Printf("I have a %v. Its name is %v. He's %v years old", pet, pet_name, pet_age)
+	
 }
