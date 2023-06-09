feff11a90f9111aece481dabd8f79a679537ea11
RageNeko26 <rneko2006@gmail.com>
2023-06-09 19:36:36 +0700

    feat: Learn new things!

--- comparison.go
+++ comparison.go
@@ -6,6 +6,12 @@ func main() {
 	var a int8 = 10
 	var b int8 = 10
 
+	var c int8 = 12
+	var d int8 = 50
+
 	var result = a == b 
 	fmt.Println(result)
+
+	fmt.Println(c > d )
+	fmt.Println( d > c)
 }
