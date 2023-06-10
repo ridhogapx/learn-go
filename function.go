005f75b8c79d5d3cd893fc6fc6d7aa253c213b47
RageNeko26 <rneko2006@gmail.com>
2023-06-10 16:18:37 +0700

    feat: Variadic and anonymous functions

--- function.go
+++ function.go
@@ -21,6 +21,25 @@ func Msg(total int) (result string) {
 	return
 }
 
+func multiVal() (string, string) {
+	return "Hello", "World"
+}
+
+// Variadic
+func exVariadic(a ...int) int {
+	num := 0
+	for _, el := range a {
+		num += el
+	}
+	return num
+}
+
+// Anonymous
+type Coba func(string) string
+func Cb(foo string,anon Coba)  {
+	anon(foo)
+}
+
 func main() {
 	msg := SayHello()
 	fmt.Println(msg)
@@ -33,4 +52,16 @@ func main() {
 
 	example := Msg(4)
 	fmt.Println(example)
+
+	_, lastWord :=  multiVal()
+	fmt.Println(lastWord)
+
+	exNumber := []int{5,10,20}
+	fmt.Println(exVariadic(2,5,1,6))
+	fmt.Println(exVariadic(exNumber...))
+
+	tanpa_nama := Cb("Hello", func(name string) string {
+		return name == "Bar"
+	})
+
 }
